cc18b1f5728985a82f6a2341f69cfe0205bf6c64
Victor Wardi <victorcaetano@live.com>
2018-06-20 22:27:11 -0300

    CAlendario

--- lib/screen/calendar_screen.dart
+++ lib/screen/calendar_screen.dart
@@ -8,6 +8,8 @@ import 'package:corrida_urbana/dao/corrida_dao.dart';
 
 import 'corrida_screen.dart';
 
+enum Estados { rio, sp }
+
 class CalendarScreen extends StatefulWidget {
   @override
   _CalendarScreenState createState() => _CalendarScreenState();
@@ -16,14 +18,20 @@ class CalendarScreen extends StatefulWidget {
 class _CalendarScreenState extends State<CalendarScreen> {
   Future<List<Corrida>> corridas;
 
-  String _estado;
+  String _estado = "RJ";
+
+  String _title = "Calendário - RJ";
+
+  List<String> _estados = ['RJ', 'SP', 'DF', 'MG'];
+
+  TextStyle _menuItemStyle =
+      TextStyle(color: Colors.teal[900], fontWeight: FontWeight.bold);
 
   @override
   void initState() {
     super.initState();
     setState(() {
-      this._estado = "RJ";
-      this.corridas = new CorridaDao().getCorridasPorEstado(_estado) as Future<List<Corrida>>;
+      this.corridas = new CorridaDao().getCorridasPorEstado(_estado);
     });
   }
 
@@ -31,7 +39,28 @@ class _CalendarScreenState extends State<CalendarScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Calendário"),
+        title: new Text(_title),
+        actions: <Widget>[
+          // overflow menu
+          new PopupMenuButton<String>(onSelected: (String estadoSelected) {
+            setState(() {
+              print(estadoSelected);
+              this.corridas =
+                  new CorridaDao().getCorridasPorEstado(estadoSelected);
+              _title = 'Calendário  - $estadoSelected';
+            });
+          }, itemBuilder: (BuildContext context) {
+            return _estados.map((String uf) {
+              return new PopupMenuItem<String>(
+                value: uf,
+                child: new Text(
+                  uf,
+                  style: _menuItemStyle,
+                ),
+              );
+            }).toList();
+          })
+        ],
       ),
       body: new Center(child: _buildScreen(context)),
     );
@@ -57,18 +86,68 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
+  Widget _corrida(Corrida corrida) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(5.0),
+              height: 50.0,
+              width: 50.0,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    corrida.dia,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    corrida.mesExtenso.substring(0, 3).toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  corrida.titulo,
+                  softWrap: true,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget _createListView(BuildContext context, List<Corrida> corridas) {
     return new Container(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.only(top: 20.0),
       child: new ListView.builder(
-        padding: new EdgeInsets.all(16.0),
         itemExtent: 80.0,
         itemCount: corridas == null ? 0 : corridas.length,
         itemBuilder: (BuildContext context, int index) {
           return new Column(
             children: <Widget>[
               new ListTile(
-                title: new Text(corridas[index].titulo),
+                title: _corrida(corridas[index]),
                 onTap: () {
                   Navigator.push(
                       context,
@@ -86,13 +165,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
       ),
       decoration: new BoxDecoration(
         gradient: new RadialGradient(
-          center: Alignment.center, 
+          center: Alignment.center,
           radius: 1.0,
           colors: [
-             Colors.blueGrey.withOpacity(0.5),
-              Colors.blueGrey.withOpacity(0.0),
+            Colors.blueGrey.withOpacity(0.5),
+            Colors.blueGrey.withOpacity(0.0),
           ],
-         
         ),
       ),
     );
