14031abaf738d612660a67b09a4015a5d0021c60
Victor Wardi <victorcaetano@live.com>
2018-07-05 16:24:49 -0300

    loading more posts

--- lib/dao/post_dao.dart
+++ lib/dao/post_dao.dart
@@ -8,27 +8,28 @@ import 'package:intl/intl.dart';
 import '../model/post.dart';
 
 class PostDao {
-
-  Future<List<Post>> getNews() async {
-    return getPosts(
-        'https://www.corridaurbana.com.br/wp-json/wp/v2/posts?&_embed&fields=title,link,date,_embedded.wp:featuredmedia',
+  Future<List<Post>> getNews(int page) async {
+    return _getPosts(
+        'https://www.corridaurbana.com.br/wp-json/wp/v2/posts?&_embed&fields=title,link,date,_embedded.wp:featuredmedia&page=${page}',
         'assets/jsons/posts.json');
   }
 
-  Future<List<Post>> getReviews() async {
-    return getPosts(
-        "https://www.corridaurbana.com.br/wp-json/wp/v2/posts?_embed&fields=title,link,date,_embedded.wp:featuredmedia&tags=66",
-        'assets/jsons/reviews.json');
+  Future<List<Post>> getReviews(int page) async {
+    return _getPosts(
+        "https://www.corridaurbana.com.br/wp-json/wp/v2/posts?_embed&fields=title,link,date,_embedded.wp:featuredmedia&tags=66&page=${page}",
+        'assets/jsons/reviews-empty.json');
+       // 'assets/jsons/reviews.json');
   }
 
-  Future<List<Post>> getPosts(String urlJsonInternet, String fileJsonLocal) async {
+  Future<List<Post>> _getPosts(
+      String urlJsonInternet, String fileJsonLocal) async {
     try {
       //read json from internet
       final responseInternet = await http.get(urlJsonInternet);
       //If server returns an OK response, parse the JSON
       return _buildPostList(responseInternet.body);
     } catch (e) {
-      print("Impossible to read Internet Json - Error: " + e.toString());
+      // print("Impossible to read Internet Json - Error: " + e.toString());
       //read json from local file
       final responseLocal = await loadLocalJson(fileJsonLocal);
       return _buildPostList(responseLocal);
@@ -36,47 +37,49 @@ class PostDao {
   }
 
   List<Post> _buildPostList(String postsJson) {
+
     List<Post> posts = new List<Post>();
 
     final responseJson = json.decode(postsJson);
 
-    for (var postJson in responseJson) {
-      Post post = new Post();
+    try {
+      for (var postJson in responseJson) {
+        Post post = new Post();
 
-      if (postJson['title']['rendered'] != null) {
-        post.title = postJson['title']['rendered'];
+        if (postJson['title']['rendered'] != null) {
+          post.title = postJson['title']['rendered'];
+        }
+        if (postJson['date'] != null) {
+          initializeDateFormatting();
+          DateTime date = DateTime.parse(postJson['date']);
+          post.date = DateFormat("d 'de' MMMM 'de' yyyy", 'pt_BR').format(date);
+        } else {
+          post.date = '';
+        }
+        if (postJson['link'] != null) {
+          post.link = postJson['link'];
+        }
+        if (postJson['_embedded'] != null) {
+          if (postJson['_embedded']['wp:featuredmedia'] !=
+              null) if (postJson['_embedded']['wp:featuredmedia']
+                  [0] !=
+              null) if (postJson['_embedded']['wp:featuredmedia'][0]
+                  ['source_url'] !=
+              null)
+            post.image =
+                postJson['_embedded']['wp:featuredmedia'][0]['source_url'];
+        }
+        if (postJson['link'] != null) {
+          post.link = postJson['link'];
+        }
+
+        posts.add(post);
+
+      
       }
-      if (postJson['date'] != null) {
-     
-        initializeDateFormatting();
-
-        DateTime date =  DateTime.parse(postJson['date']);
-
-         post.date = DateFormat("d 'de' MMMM 'de' yyyy", 'pt_BR').format(date); 
-
-         print(post.date);
-
-;
-      }else{
-        post.date = '';
-      }
-      if (postJson['link'] != null) {
-        post.link = postJson['link'];
-      }
-      if (postJson['_embedded'] != null) {
-        if (postJson['_embedded']['wp:featuredmedia'] != null)
-          if (postJson['_embedded']['wp:featuredmedia'][0] != null)
-            if (postJson['_embedded']['wp:featuredmedia'][0]['source_url'] != null)      
-              post.image = postJson['_embedded']['wp:featuredmedia'][0]['source_url'];
-      }
-      if (postJson['link'] != null) {
-        post.link = postJson['link'];
-      }
-     
-
-      posts.add(post);
+    } catch (e) {
+      return posts; //return empty list
     }
-
     return posts;
   }
 
--- lib/screen/posts_screen.dart
+++ lib/screen/posts_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:corrida_urbana/model/post.dart';
 import 'package:flutter/material.dart';
 
 import 'package:url_launcher/url_launcher.dart';
@@ -17,26 +18,98 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
-  
-
   Future<List> postsInternet;
+  List<Post> posts;
+
+  String shareDescription;
+  String readMoreText;
+
+  ScrollController _scrollController = new ScrollController();
+  bool isPerformingRequest = false;
+
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
-    setState(() {     
-      this.postsInternet = widget.postType == 'news' ? new PostDao().getNews() : new PostDao().getReviews();
+    setState(() {
+      if (widget.postType == 'news') {
+        this.postsInternet = new PostDao().getNews(1);
+        new PostDao().getNews(1).then((posts) {
+          this.posts = posts;
+        });
+        this.shareDescription = 'Confira mais no site Corrida Urbana: ';
+        this.readMoreText = ' LER NOTÍCIA COMPLETA';
+      } else if (widget.postType == 'reviews') {
+        this.postsInternet = new PostDao().getReviews(1);
+        new PostDao().getReviews(1).then((posts) {
+          this.posts = posts;
+        });
+        this.shareDescription = 'Confira o review no site Corrida Urbana: ';
+        this.readMoreText = ' LER REVIEW COMPLETO';
+      }
+
+      _scrollController.addListener(() {
+        if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent) {
+          _getMoreData();
+        }
+      });
     });
   }
 
-   @override
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _getMoreData() async {
+    if (!isPerformingRequest) {
+      setState(() => isPerformingRequest = true);
+      this.page++;
+
+      List<Post> morePosts = new List<Post>();
+      if (widget.postType == 'news1') {
+        morePosts = await new PostDao().getNews(this.page);
+      } else if (widget.postType == 'reviews') {
+        morePosts = await new PostDao().getReviews(this.page);
+      }
+
+  if (morePosts.isEmpty) {
+      double edge = 50.0;
+      double offsetFromBottom = _scrollController.position.maxScrollExtent - _scrollController.position.pixels;
+      if (offsetFromBottom < edge) {
+        _scrollController.animateTo(
+            _scrollController.offset - (edge -offsetFromBottom),
+            duration: new Duration(milliseconds: 500),
+            curve: Curves.easeOut);
+      }
+    }
+
+      setState(() {
+        posts.addAll(morePosts);
+        isPerformingRequest = false;
+      });
+    }
+  }
+
+  Widget _buildProgressIndicator() {
+    return  new Center(
+          child: new Opacity(
+            opacity: isPerformingRequest ? 1.0 : 0.0,
+            child: new CircularProgressIndicator(),
+          ),      
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(child: _buildScreen(context)),
-    );
+        appBar: new AppBar(
+          title: new Text(widget.title),
+        ),
+        body: Center(child: _buildScreen(context)));
   }
 
   Widget _buildScreen(BuildContext context) {
@@ -53,7 +126,11 @@ class _PostsScreenState extends State<PostsScreen> {
             else
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: _createListView(context, snapshot.data),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(child: _createListView(context, snapshot.data)),                    
+                  ],
+                ),
               );
         }
       },
@@ -61,87 +138,114 @@ class _PostsScreenState extends State<PostsScreen> {
   }
 
   Widget _createListView(BuildContext context, List posts) {
+    posts = this.posts;
     return ListView.builder(
       padding: EdgeInsets.all(0.0),
+      controller: _scrollController,
       //itemExtent: 380.0,
-      itemCount: posts == null ? 0 : posts.length,
+      itemCount: posts == null ? 0 : posts.length + 1,
       itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: <Widget>[
-            Card(
-              child: Column(
-                // mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              width: double.infinity,
-                              height: 200.0,
-                              child: posts[index].image != null
-                                  ? Image.network(posts[index].image,
-                                      fit: BoxFit.fitWidth)
-                                  : Image.asset('assets/images/temp.png',
-                                      fit: BoxFit.fitWidth),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(6.0),
-                              child: Text(
-                                posts[index].title,
-                                softWrap: true,
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
+print(index);
+        if (index == (posts.length)) {
+          //return Center(child: new CircularProgressIndicator());
+         return _buildProgressIndicator();
+        } else {
+        var titulo = '${index + 1} - ${posts[index].title} ';
+          return Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  // mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: 200.0,
+                                child: posts[index].image != null
+                                    ? Image.network(posts[index].image,
+                                        fit: BoxFit.fitWidth)
+                                    : Image.asset('assets/images/temp.png',
+                                        fit: BoxFit.fitWidth),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: posts[index].date != '' ? Text(
-                                posts[index].date,                                
-                                style: TextStyle(
-                                    fontSize: 12.0,               ),
-                              ) : Container(width: 0.0, height: 0.0,),
-                            ),
-                            Row(
-                              children: <Widget>[
-                                FlatButton(
-                                  child: Row(
-                                    children: <Widget>[
-                                      Icon(Icons.playlist_play),
-                                      Text('Ver mais'),
-                                    ],
-                                  ),
-                                  onPressed: () {
-                                    _launchURL(posts[index].link);
-                                  },
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20.0),
+                                child: Text(
+                                  titulo,
+                                  softWrap: true,
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                FlatButton(
-                                  child: Icon(Icons.share),
-                                  onPressed: () {
-                                    var link = posts[index].link;
-                                    Share.share(
-                                        'Confira o review no site Corrida Urbana: $link');
-                                  },
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: posts[index].date != '' &&
+                                        widget.postType == 'news'
+                                    ? Text(
+                                        posts[index].date,
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      )
+                                    : Container(
+                                        width: 0.0,
+                                        height: 0.0,
+                                      ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    FlatButton(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Row(
+                                        children: <Widget>[
+                                          Icon(Icons.add_circle),
+                                          Text(this.readMoreText),
+                                        ],
+                                      ),
+                                      onPressed: () {
+                                        _launchURL(posts[index].link);
+                                      },
+                                    ),
+                                    Center(
+                                      child: FlatButton(
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Icon(Icons.share),
+                                        onPressed: () {
+                                          var link = posts[index].link;
+                                          Share.share(
+                                              this.shareDescription + link);
+                                        },
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        );
+            ],
+          );
+        }
       },
     );
   }
+
 
   _launchURL(String url) async {
     if (await canLaunch(url)) {
