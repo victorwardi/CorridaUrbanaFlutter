66cb082d3e6ab8c94de6d140183766ea8de4911f
Victor Wardi <victorcaetano@live.com>
2018-07-17 16:25:10 -0300

    converter

--- lib/screen/converter_screen.dart
+++ lib/screen/converter_screen.dart
@@ -1,8 +1,5 @@
-import 'package:corrida_urbana/util/custom_decoration.dart';
 import 'package:corrida_urbana/util/custom_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-
 
 class ConverterPaceScreen extends StatefulWidget {
   @override
@@ -10,81 +7,171 @@ class ConverterPaceScreen extends StatefulWidget {
 }
 
 class _ConverterScreenState extends State<ConverterPaceScreen> {
-  final List<ItemSwiper> distancias = [
-    ItemSwiper('1k', 1000),
-    ItemSwiper('5k', 5000),
-    ItemSwiper('10k', 10000),
-    ItemSwiper('Meia-maratona', 21097),
-    ItemSwiper('Maratona', 42195),
+  int hours = 0;
+  int mins = 0;
+  int secs = 0;
+  ItemSwiper distance = ItemSwiper('1k', 1.0);
+
+  bool showResult = false;
+
+  final List<ItemSwiper> distances = [
+    ItemSwiper('1k', 1.0),
+    ItemSwiper('5k', 5.0),
+    ItemSwiper('10k', 10.0),
+    ItemSwiper('Meia-maratona', 21.097),
+    ItemSwiper('Maratona', 42.195),
   ];
+
+  List<ItemSwiper> _getTimes() {
+    List<ItemSwiper> times = [];
+    for (var i = 0; i < 60; i++) {
+      String n = '$i'.padLeft(2, '0');
+      times.add(ItemSwiper('$n', i));
+    }
+    return times;
+  }
+  List<DropdownMenuItem> _getTimesDropDown() {
+    List<DropdownMenuItem> times = [];
+    for (var i = 0; i < 60; i++) {
+      String n = '$i'.padLeft(2, '0');
+      times.add(DropdownMenuItem(value: i, child: Text(n),));
+    }
+    return times;
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<ItemSwiper> times = _getTimes();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Infinity Loop Items'),
       ),
       body: Column(
         children: <Widget>[
-          _buildSwiperField(distancias, 'Distâncias: '),
-          _buildSwiperField(distancias),
-          _buildSwiperField(distancias),
-          _buildSwiperField(distancias),
-          _buildSwiperField(distancias),
+          DropdownButton(items:_getTimesDropDown() ,),
+          CustomSwiper(
+            list: distances,
+            width: 560.0,
+            label: 'Distâncias:',
+            scrollDirection: Axis.horizontal,
+            onIndexChanged: (index) => this.distance = distances[index],
+          ),
+          Container(
+           padding: EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CustomSwiper(
+                  list: times,
+                  unit: 'Hours',
+                  width: 100.0,
+                  fontSize: 30.0,
+                  onIndexChanged: (index) => this.hours = times[index].value,
+                ),
+                CustomSwiper(
+                  list: times,
+                  unit: 'Mins',
+                  width: 100.0,
+                  fontSize: 30.0,
+                  onIndexChanged: (index) => this.mins = times[index].value,
+                ),
+                CustomSwiper(
+                  list: times,
+                  unit: 'Secs',
+                  width: 100.0,
+                  fontSize: 30.0,
+                  onIndexChanged: (index) => this.secs = times[index].value,
+                ),
+              ],
+            ),
+          ),
+          FlatButton(
+              padding: EdgeInsets.all(20.0),
+              child: Container(
+                  color: Colors.teal,
+                  width: double.infinity,
+                  child: Icon(
+                    Icons.compare_arrows,
+                    size: 60.0,
+                  )),
+              onPressed: () {
+                setState(() {
+                  showResult = true;
+                });
+              }),
+          showResult ? result() : Container()
         ],
-      ),  
-    
+      ),
     );
   }
 
-   Widget swiper(List list) {
-  return  Swiper(
-        itemCount: list.length,
-     viewportFraction: 0.3,
-     outer: true,
-     scale: 0.5,
-        scrollDirection: Axis.vertical,
-        physics: FixedExtentScrollPhysics(),
-        control: SwiperControl(
-            size: 20.0, color: Colors.grey, padding: EdgeInsets.all(0.0)),
-        itemBuilder: (BuildContext context, int index) {
-          return Center(
-            child: Container(
-              child: Text(
-                list[index].label,
-                style: TextStyle(fontSize: 26.0),
-              ),
-            ),
-          );
-        },
-  );
-
-
-}
-
-  _buildSwiperField( List list, [String label]) {
-
- return Row(
-
-   children: <Widget>[
-label != null ? Text('Distancias:') :Container() ,
-  
-          Container(
-            height: 200.0,
-              width: 200.0,             
-              padding: EdgeInsets.all(10.0),
-              child: 
-               swiper(distancias),
-              )
-   ],
- );
+  void getTime() {
+    print('$hours:$mins:$secs');
   }
 
-}
+  ///Convert distance and time to pace per km
+  Widget result() {
+    if (this.hours == 0.0 && this.mins == 0.0 && this.secs == 0.0) {
+      return Text(
+        'Informe o tempo!',
+        style: TextStyle(color: Colors.red, fontSize: 25.0),
+      );
+    }
 
-class ItemSwiper {
-  int value;
-  String label;
+    //convert hours to minutes
+    int hoursToMinutes = this.hours * 60 * 60;
 
-  ItemSwiper(this.label, this.value);
+    //convert seconds to minutes
+    double secondsToMinutes = this.secs / 60;
+
+    //Get total of seconds
+    double totalMinutes = hoursToMinutes + this.mins + secondsToMinutes;
+
+    //Calculate total of minutes per kilometer
+    double minutesPerKm = totalMinutes / this.distance.value;
+    //Get only integer part of minutes
+    int minutesPerKmIntegers = minutesPerKm.toInt();
+
+    if(minutesPerKmIntegers > 59){
+       return Text(
+        'Informe uma distância maior!',
+        style: TextStyle(color: Colors.red, fontSize: 25.0),
+      );
+    }
+
+    //Get the rest part of kilometers   
+    double minutesPerKmRest = minutesPerKmIntegers > 0 ? minutesPerKm % minutesPerKmIntegers : minutesPerKm;
+
+    
+
+    //Convert the rest into seconds and round it
+    int secondsMinutesPerKm = (minutesPerKmRest * 60).round();
+
+    //Format string o have a zero before an unique number
+    String secondsFormatted = '$secondsMinutesPerKm'.padLeft(2, '0');
+
+TextStyle styleDescription = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+TextStyle styleResult = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+
+    return Card(
+      margin: EdgeInsets.all(20.0),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        width: double.infinity,
+        height: 200.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/temp.png'),
+                fit: BoxFit.fitWidth),color: Colors.lightGreenAccent.withOpacity(0.1)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Seu Pace para ${this.distance.label} é: ', style: styleDescription,),
+            Text('$minutesPerKmIntegers:$secondsFormatted min/km' , style: styleResult,),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/util/custom_swiper.dart
+++ lib/util/custom_swiper.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-class CustomComponents extends StatelessWidget {
+class CustomSwiper extends StatelessWidget {
+
+  final List list;
+  final String label;
+  final String unit;
+  final double width;
+  final double height;
+  final double fontSize;
+  final ValueChanged<int> onIndexChanged;
+    final Axis scrollDirection;
+  
+
+  CustomSwiper({
+    this.list,
+    this.label,
+    this.unit,
+    this.width = 200.0,
+    this.height = 200.0,
+    this.fontSize = 25.0,
+    this.onIndexChanged,
+    this.scrollDirection = Axis.vertical
+
+
+  });
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Row(
+      children: <Widget>[
+        label != null ? Text(label) : Container(),
+        Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.all(10.0),
+          child: swiper(list),
+        ),
+        unit != null ? Text(unit) : Container(),
+      ],
     );
   }
 
 
+    Widget swiper(List list) {
+    return Swiper(
+    
+      itemCount: list.length,
+      viewportFraction: 0.3,
+      outer: true,
+      scale: 0.5,
+      scrollDirection: this.scrollDirection,
+      physics: FixedExtentScrollPhysics(),
+      control: SwiperControl(
+          size: 20.0, color: Colors.grey, padding: EdgeInsets.all(0.0)),
+      itemBuilder: (BuildContext context, int index) {
+        return Center(
+          child: Container(
+            child: Text(
+              list[index].label,
+              style: TextStyle(fontSize: fontSize),
+            ),
+          ),
+        );
+      }
+      , onIndexChanged: onIndexChanged
+    );
+  }
+
 
 }
+
+
+
+
+
+
+class ItemSwiper {
+  var value;
+  var label;
+
+  ItemSwiper(this.label, this.value);
+}
+
 
 
